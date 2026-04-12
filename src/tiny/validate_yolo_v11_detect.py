91a946f32ba0f46b2886c3c179de948aada7fdba
Christophe Pantel <ChristophePantel@users.noreply.github.com>
2026-04-12 22:37:07 +0200

    Cleaning threshold.

--- src/tiny/validate_yolo_v11_detect.py
+++ src/tiny/validate_yolo_v11_detect.py
@@ -6,16 +6,20 @@ path = "/data/christophe/hierarchical/semantic-PASCAL-Part/src/tiny/"
 # Load a pretrained YOLO model (recommended for training)
 model = YOLO(path + "results/augmented/train_with_scores_metrics_0/train/weights/best.pt")
 
-metrics_threshold = 3
+metrics_threshold = 0
 
 # Train the model using the 'coco8.yaml' dataset for 3 epochs
 results = model.val(
     data=path + "SemanticPascalPart.yaml", 
+    save_txt=True,
+    save_conf=True,
+    save_json=True,
+    visualize=True,
     use_scores=True, # (bool) use class scores instead of class labels
     use_km=True, # (bool) use knowledge model if available
     use_km_scores=True, # (bool) use knowledge model scores if available
     use_km_metrics=True, # (bool) use knowledge model metrics -- class compatibility matrix -- if available
-    km_metrics_amount=metrics_threshold, # (int) number of knowledge model metrics produced -- thresholf for the class compatibility metrics
+    km_metrics_threshold=metrics_threshold, # (int) number of knowledge model metrics produced -- thresholf for the class compatibility metrics
     use_variant_selection=True, # (bool) select main class based on knowledge model scores if available
     use_km_losses=False, # (bool) use knowledge model losses if available
     use_refinement=False, # (bool) use refinement relation if available
@@ -30,3 +34,5 @@ results = model.val(
     project= path + "results/augmented/validate_with_scores_metrics_"+ str(metrics_threshold),
     name="val",
     )
+
+pass
