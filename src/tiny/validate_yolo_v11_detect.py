9b69390718901174c3af2a3896476cc0ae1712ed
Christophe Pantel <ChristophePantel@users.noreply.github.com>
2026-04-12 12:05:05 +0200

    Cleaning drivers to produce separated results.

--- src/tiny/validate_yolo_v11_detect.py
+++ src/tiny/validate_yolo_v11_detect.py
@@ -3,18 +3,14 @@ from ultralytics import YOLO
 
 path = "/data/christophe/hierarchical/semantic-PASCAL-Part/src/tiny/"
 
-# Create a new YOLO model from scratch
-model = YOLO("yolo11n.yaml")
-
 # Load a pretrained YOLO model (recommended for training)
-model = YOLO("yolo11n.pt")
+model = YOLO(path + "results/augmented/train_with_scores_metrics_0/train/weights/best.pt")
 
-metrics_threshold = 0
+metrics_threshold = 3
 
 # Train the model using the 'coco8.yaml' dataset for 3 epochs
-results = model.train(
-    data=path+"SemanticPascalPart.yaml", 
-    epochs=1000, 
+results = model.val(
+    data=path + "SemanticPascalPart.yaml", 
     use_scores=True, # (bool) use class scores instead of class labels
     use_km=True, # (bool) use knowledge model if available
     use_km_scores=True, # (bool) use knowledge model scores if available
@@ -22,7 +18,6 @@ results = model.train(
     km_metrics_amount=metrics_threshold, # (int) number of knowledge model metrics produced -- thresholf for the class compatibility metrics
     use_variant_selection=True, # (bool) select main class based on knowledge model scores if available
     use_km_losses=False, # (bool) use knowledge model losses if available
-    km=100,
     use_refinement=False, # (bool) use refinement relation if available
     km_specialization_weight=1.0, # (float) weight of the specialization loss
     km_specialization_exclusion_weight=1.0, # (float) weight of the specialization exclusion loss
@@ -32,27 +27,6 @@ results = model.train(
     km_composition_exclusion_weight=1.0, # (float) weight of the composition exclusion loss
     km_decomposition_weight=1.0, # (float) weight of the decomposition loss
     km_decomposition_exclusion_weight=1.0, # (float) weight of the decomposition exclusion loss
-    project=path + "results/augmented/train_with_scores_metrics_"+ str(metrics_threshold),
-    name="train",
-    # hsv_h=0.0,
-    # hsv_s=0.0,
-    # hsv_v=0.0,
-    # translate=0.0,
-    # scale=0.0,
-    # fliplr=0.0,
-    # mosaic=0.0,
-    # erasing=0.0,
-    # auto_augment=None,
-    )
-
-# Evaluate the model's performance on the validation set
-results = model.val(
-    project=path + "results/augmented/train_with_scores_metrics_"+ str(metrics_threshold), 
+    project= path + "results/augmented/validate_with_scores_metrics_"+ str(metrics_threshold),
     name="val",
     )
-
-# Perform object detection on an image using the model
-# results = model("https://ultralytics.com/images/bus.jpg")
-
-# Export the model to ONNX format
-# success = model.export(format="onnx")
